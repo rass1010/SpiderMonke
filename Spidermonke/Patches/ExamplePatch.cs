5a1dd0c62397d9fdec2d3aa8b94a3cc0019aa34f
rass1010 <62247959+rass1010@users.noreply.github.com>
2022-04-03 14:01:47 +0200

    Add files via upload

---
+++ Spidermonke/Patches/ExamplePatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace spidermonke_v2.Patches
+{
+    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch("Update", MethodType.Normal)]
+    
+    public class ExamplePatch : MonoBehaviour
+    {
+
+
+
+    }
+}
---
+++ Spidermonke/PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace spidermonke_v2
+{
+    class PluginInfo
+    {
+        public const string GUID = "com.rass1010.gorillatag.spidermonke_v2";
+        public const string Name = "SpiderMonke_v2";
+        public const string Version = "1.0.1";
+    }
+}
